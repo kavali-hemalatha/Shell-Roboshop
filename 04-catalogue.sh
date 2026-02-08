c145044641a90eb0554deacf0842675678023df3
Kavali Hemalatha <hemalathakavali1998@gmail.com>
2026-02-08 15:51:24 +0530

    actions

---
+++ 04-catalogue.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[39m'
+
+user_id=$(id -u)
+log_folder="/var/log/Shell-Roboshop"
+log_file="$log_folder/$0.log"
+
+if [ $user_id -ne 0 ]; then
+echo -e "this script needs to be run with $R root $N user" | tee -a $log_file
+exit 1
+fi
+
+mkdir -p $log_folder
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+echo -e "$R $2.....FAILURE" | tee -a $log_file
+exit 1
+else
+echo -e "$G $2.....SUCCESS" | tee -a $log_file
+fi
+}
+
+dnf module disable nodejs -y &>>$log_file
+VALIDATE $? "disabling nodejs default version"
+
+dnf module enable nodejs:20 -y &>>$log_file
+VALIDATE $? "enabling nodejs 20 version"
+
+dnf install nodejs -y &>>$log_file
+VALIDATE $? "installing nodejs"
+
+id roboshop &>>$log_file
+if [ id -ne 0 ];then
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+   VALIDATE $? "creating system user"
+else
+    echo -e "system user already exist ... $Y skipping $N
+
+mkdir -p /app
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+cd /app 
+unzip /tmp/catalogue.zip
