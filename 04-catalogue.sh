f2441e9c382b96ff435e07bcbe423a753db4c804
Kavali Hemalatha <hemalathakavali1998@gmail.com>
2026-02-08 21:13:21 +0530

    actions

--- 04-catalogue.sh
+++ 04-catalogue.sh
@@ -76,5 +76,14 @@ VALIDATE $? "starting and enabling catalogue service"
 cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 
-mongosh --host $MONGODB_HOST </app/db/master-data.js
-mongosh --host $MONGODB_HOST
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue 
+VALIDATE $? "Restarting catalogue"
